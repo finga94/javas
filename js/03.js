6c86cda950594341b92bc663f807f1590c7a901a
finga94 <131739400+finga94@users.noreply.github.com>
2024-01-11 22:20:46 -0500

    Add files via upload
    Signed-off-by: finga94 <131739400+finga94@users.noreply.github.com>

--- js/03.js
+++ js/03.js
@@ -1,9 +1,12 @@
-let x = 1;
-let suma = 0;
-let valor;
-while (x <= 5) {
-    valor = parseInt(prompt('Ingrese valor:'));
-    suma = suma + valor;
-    x = x + 1;
-}
-document.write('La suma de los valores es ' + suma + '<br>');
+/*Escribir un programa que lea 10 notas de alumnos y nos informe cuántos tienen notas mayores o
+ iguales a 7 y cuántos menores.*/
+let x = 0;
+        let suma = 0;
+        while (x < 5) {
+            let altura;
+            altura = parseInt(prompt('Ingrese la altura en centímetros(Ej. 175)'));
+            suma = suma + altura;
+            x = x + 1;
+        }
+        let promedio = suma / 5;
+        document.write('La altura promedio de las cinco personas es:' + promedio);
